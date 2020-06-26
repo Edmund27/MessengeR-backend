ca6682688bd262b3fe88470bbdc4ea4d55e47237
edmund27 <epafko@gmail.com>
2020-06-26 11:53:12 +0200

    set up database and auth

--- index.js
+++ index.js
@@ -19,3 +19,54 @@ io.on('connection', (socket) => {
         io.emit('chat', messageObject)
     })
 })
+
+// SERVER:
+const loggerMiddleWare = require("morgan");
+const authRouter = require("./routers/auth");
+const authMiddleWare = require("./auth/middleware");
+
+app.use(loggerMiddleWare("dev"));
+
+const bodyParserMiddleWare = express.json();
+app.use(bodyParserMiddleWare);
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("Hi from express");
+});
+
+// POST endpoint for testing purposes, can be removed
+app.post("/echo", (req, res) => {
+    res.json({
+        youPosted: {
+            ...req.body,
+        },
+    });
+});
+
+app.post("/authorized_post_request", authMiddleWare, (req, res) => {
+    // accessing user that was added to req by the auth middleware
+    const user = req.user;
+    // don't send back the password hash
+    delete user.dataValues["password"];
+
+    res.json({
+        youPosted: {
+            ...req.body,
+        },
+        userFoundWithToken: {
+            ...user.dataValues,
+        },
+    });
+});
+
+app.use("/", authRouter);
+
+// Listen for connections on specified port (default is port 4000)
+
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`);
+});
+
+
