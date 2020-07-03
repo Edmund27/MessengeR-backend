e63473867aeb82140362d37360f9331b01ab624a
edmund27 <epafko@gmail.com>
2020-07-03 12:41:54 +0200

    listening to credentials from client and adding to database

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const socket = require('socket.io')
 const app = express()
 const cors = require('cors')
 const PORT = 4000
+const User = require('./models').user;
+const Chat = require('./models').chat;
+const Message = require('./models').message;
+const mobileSockets = {}
 
 app.use(cors());
 
@@ -12,6 +16,20 @@ const server = app.listen(PORT, () => {
 
 const io = socket(server);
 io.on('connection', (socket) => {
+    socket.on('newUser', async credentials => {
+        const { name, password } = credentials
+        console.log("CREDENTIALS:", name, password)
+        const user = await User.findOrCreate({
+            where: {
+                name,
+                password
+            }
+        })
+        const users = await User.findAll()
+        console.log("USERS:", user, users)
+        socket.emit('userCreated', { user: user[0], users })
+    })
+
     console.log(`connection established via id: ${socket.id}`)
 
     socket.on('chat', messageObject => {
